b4778a5c5047c5c724abdacea6f1d1bf10aee7cd
kenoneffe <fbneffe@addu.edu.ph>
2018-12-05 15:38:23 +0800

    added WIP timer

--- app/src/main/java/com/example/fbneffe/neffesecondapk/Second.java
+++ app/src/main/java/com/example/fbneffe/neffesecondapk/Second.java
@@ -1,13 +1,60 @@
 package com.example.fbneffe.neffesecondapk;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
 
 public class Second extends AppCompatActivity {
+
+    private EditText midEdit;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondactivity);
+
+
+        midEdit = (EditText) findViewById(R.id.midEditView);
+
+        timer();
+
+
+
     }
+
+    public void timer(){
+
+        new CountDownTimer(4000, 100){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+
+                midEdit.setText("0" + millisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+
+                midEdit.setText("00");
+
+            }
+        }.start();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
