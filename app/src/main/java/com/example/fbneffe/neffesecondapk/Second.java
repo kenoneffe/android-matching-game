c6659eea9b5714a8e4ce880065f67430e4251fc0
Aji <jwpbanawan@addu.edu.ph>
2018-12-09 16:17:04 +0800

    Implemented showAll() and hideAll() function. Generate random fix.

--- app/src/main/java/com/example/fbneffe/neffesecondapk/Second.java
+++ app/src/main/java/com/example/fbneffe/neffesecondapk/Second.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 
@@ -21,6 +23,8 @@ public class Second extends AppCompatActivity implements View.OnClickListener {
             R.id.button12,R.id.button13,R.id.button14,R.id.button15,
     };
 
+    private String []button_storage = new String[16];
+
     // For countdown timer
     private TextView midEdit;
 
@@ -32,10 +36,12 @@ public class Second extends AppCompatActivity implements View.OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.secondactivity);
 
+
         // Timer Variables
         midEdit = (TextView) findViewById(R.id.countdown);
         timer();
         alphabetRandomizer();
+        showAll();
 
         //Button Listener Initialization
         Button one = (Button) findViewById(R.id.button0);
@@ -80,16 +86,13 @@ public class Second extends AppCompatActivity implements View.OnClickListener {
 
             @Override
             public void onTick(long millisUntilFinished) {
-
-
                 midEdit.setText("0" + millisUntilFinished / 1000);
             }
 
             @Override
             public void onFinish() {
-
                 midEdit.setText("00");
-
+                hideAll();
             }
         }.start();
 
@@ -99,29 +102,26 @@ public class Second extends AppCompatActivity implements View.OnClickListener {
     public void alphabetRandomizer(){
 
         String []arr = this.getResources().getStringArray(R.array.alphabet);
-        boolean[] chosen_already = new boolean[8];
+        boolean[] chosen_already = new boolean[16];
         String strr = "";
 
-        for (int i = 0; i < 8; i++) {
+        for (int i = 0; i < 16; i++) {
             Button btn = findViewById(buttons[i]);
             // do random here
             Random r = new Random();
-            int num = r.nextInt(8);
+            int num = r.nextInt(16);
 
-
-            if (!chosen_already[num]) {
-
-                btn.setText(arr[num] + " -? " + num);
-                chosen_already[num] = true;
-
-            } else {
-                    btn.setText("  -? " + num);
-
-            }
-
-
+            do{
+                if(chosen_already[num]){
+                    num = r.nextInt(16);
+                }
+                if (!chosen_already[num]) {
+                    button_storage[i] = arr[num];
+                    chosen_already[num] = true;
+                    break;
+                }
+            }while(chosen_already[num]);
         }
-
     }
 
     public void toggleButton(TextView v){
@@ -131,82 +131,33 @@ public class Second extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View view) {
         TextView currButtText = (TextView) findViewById(view.getId());
-
-        currButtText.setText(getIndex(view) + "");
-
-        /*
-        switch (view.getId()) {
-            case R.id.button0:
-                toggleButton((TextView) findViewById(buttons[0]));
-                break;
-
-            case R.id.button1:
-                // do your code
-                break;
-
-            case R.id.button2:
-                // do your code
-                break;
-            case R.id.button3:
-                // do your code
-                break;
-
-            case R.id.button4:
-                // do your code
-                break;
-            case R.id.button5:
-                // do your code
-                break;
-
-            case R.id.button6:
-                // do your code
-                break;
-            case R.id.button7:
-                // do your code
-                break;
-
-            case R.id.button8:
-                // do your code
-                break;
-            case R.id.button9:
-                // do your code
-                break;
-
-            case R.id.button10:
-                // do your code
-                break;
-            case R.id.button11:
-                // do your code
-                break;
-
-            case R.id.button12:
-                // do your code
-                break;
-            case R.id.button13:
-                // do your code
-                break;
-            case R.id.button14:
-                // do your code
-                break;
-            case R.id.button15:
-                // do your code
-                break;
-            default:
-                break;
-        }*/
+        currButtText.setText(button_storage[getIndex(view)]);
     }
 
     private int getIndex(View view){
-
         for(int i = 0; i < buttons.length; i++){
             if(buttons[i] == view.getId()){
                 return i;
             }
         }
-
         return 0;
     }
 
+    private void showAll(){
+        for(int i=0; i<16; i++){
+            Button btn = (Button) findViewById(buttons[i]);
+            btn.setText(button_storage[i]);
+            btn.setEnabled(false);
+        }
+    }
+
+    private void hideAll(){
+        for(int i=0; i<16; i++){
+            Button btn = (Button) findViewById(buttons[i]);
+            btn.setText("???");
+            btn.setEnabled(true);
+        }
+    }
 
 
 }
